6d632f95f4e7b0b728c7a782f2f10b4ee6a7bbb3
Larry Leathers <lleathers@naehe.com>
2019-10-23 10:52:56 -0400

    We simplified authentication to just google sign-in to facilitate development. We require authentication for access to real resources now. Now you can see button for logging in, it informs present login status and provides a sign out button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { auth } from 'firebase/app';
-
+import * as firebaseui from 'firebaseui';
 
 const yourFirebaseConfig = {
   apiKey: "AIzaSyC7KRPdXjqQIhNKhcY_98vrkfQZyU2gHIw",
@@ -80,7 +80,55 @@ export class AppComponent {
     this.points.subscribe(console.log);
   }
 
+onSignIn(googleUser) {
+  console.log('Google Auth Response', googleUser);
+  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+  var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+    unsubscribe();
+    // Check if we are already signed-in Firebase with the correct user.
+    if (!this.isUserEqual(googleUser, firebaseUser)) {
+      // Build Firebase credential with the Google ID token.
+      var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.getAuthResponse().id_token);
+      // Sign in with credential from the Google user.
+      firebase.auth().signInWithCredential(credential).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    } else {
+      console.log('User already signed-in Firebase.');
+    }
+  });
+}
 
+
+isUserEqual(googleUser, firebaseUser) {
+  if (firebaseUser) {
+    var providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+        // We don't need to reauth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+
+//signOut() {
+//    var auth2 = gapi.auth2.getAuthInstance();
+//    auth2.signOut().then(function () {
+//      console.log('User signed out.');
+//    });
+//  }
 
 
   // Logic for dismissing and recovering form for map interaction
